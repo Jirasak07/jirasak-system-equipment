66534c3207ccc9aeb1b53f0e193fa8e971c8b004
Jirasak07 <bee.110243@gmail.com>
2023-04-21 10:44:11 +0700

    save

--- project-final/src/Component/Chart/BarChart.js
+++ project-final/src/Component/Chart/BarChart.js
@@ -26,6 +26,57 @@ function BarChart() {
   const month = new Date(date).getMonth() + 1;
   const currentYear = new Date(date).getFullYear();
   const [lb, setLb] = useState([]);
+
+  const main_aid = localStorage.getItem("main_aid");
+  const [countsub, setCountSub] = useState();
+  const [datas, setDatas] = useState();
+  const [dataArray, setDataArray] = useState([]);
+  var f = []
+  var l = [2,5,3,5,4]
+  useEffect(() => {
+    console.log("use BarChart");
+    axios
+      .post("http://localhost:4444/barchart", {
+        main_aid: main_aid,
+      })
+      .then((res) => {
+        setDataArray(res.data);
+      });
+    axios
+      .post("http://localhost:4444/countsub", {
+        main_aid: main_aid,
+      })
+      .then((res) => {
+        setCountSub(res.data[0].sub);
+      });
+    return () => {
+      console.log("out Barchart");
+    };
+  }, []);
+  useEffect(() => {
+    for(let i=1;i<=countsub;i++){
+      dataArray.forEach((item,index)=>{
+        if(item.sub == 1){
+          f.push(item.qty)
+        }
+     
+      })
+      console.log(f)
+    }
+  });
+  
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'จำนวนครุภัณฑ์แยกตามหน่วยงาน',
+      },
+    },
+  };
   const labels = [
     "งานเลขาณุการ",
     "งานทะเบียนและประมวลผล",
@@ -33,102 +84,24 @@ function BarChart() {
     "งานเทคโนโลยีสารสนเทศ",
     "ศูนย์พิจิตร",
   ];
-  const options = {
-    responsive: true,
-    maintainAspecRadio: false,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: "ครุภัณฑ์แยกตามหน่วยงาน ประจำปีงบประมาณ "+fisiyear,
-      },
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  };
-  const main_aid = localStorage.getItem("main_aid");
-  const [countsub, setCountSub] = useState();
-  const [datas, setDatas] = useState();
-  const [dataArray, setDataArray] = useState([]);
-  useEffect(() => {
-    console.log("use BarChart");
-    axios.post("http://localhost:4444/countsub", {
-        main_aid: main_aid,
-      })
-      .then((res) => {
-        setCountSub(res.data[0].sub);
-      });
-    // for (let i = 1; i <= countsub; i++) {
-    //   axios
-    //     .post("http://localhost:4444/barchart", {
-    //       pstatus_id: 1,
-    //       sub_aid: i,
-    //     })
-    //     .then((res) => {
-    //       normal.push(res.data[0].data);
-    //     });
-    //   setDatas(normal);
-    // }
-    return () => {
-      console.log("out Barchart");
-    };
-  }, []);
-  useEffect(()=>{
-    if (month >= 10) {
-      setFisiYear(currentYear + 544);
-    } else {
-      setFisiYear(currentYear + 543);
-    }
-    console.log("count2 : "+countsub)
-    
-  })
-  
   const data = {
-    labels,
-    datasets: [
-      {
-        label: "ใช้งานปกติ",
-        data: datas,
-        backgroundColor: "#8cd867",
-        borderWidth: 2,
-        borderColor: "#8cd867",
-      },
-      {
-        label: "ชำรุด(รอซ่อมบำรุง)",
-        data: [4, 16, 37, 81, 15],
-        backgroundColor: "#ffd000",
-        borderWidth: 2,
-        borderColor: "#ffd000",
-      },
-      {
-        label: "บริจาค",
-        data: [15, 16, 3, 5, 14],
-        backgroundColor: "#4895ef",
-        borderWidth: 2,
-        borderColor: "#4895ef",
-      },
-      {
-        label: "เสื่อมคุณภาพ(ไม่จำเป็นต้องใช้ในราชการ)",
-        data: [6, 4, 7, 8, 15],
-        backgroundColor: "#dd2d4a",
-        borderWidth: 2,
-        borderColor: "#dd2d4a",
-      },
-      {
-        label: "จำหน่ายออกจากบัญชี",
-        data: [12, 13, 4, 18, 19],
-        backgroundColor: "#c77dff",
-        borderWidth: 2,
-        borderColor: "#c77dff",
-      },
-    ],
+    labels: labels,
+    datasets: [{
+      label:'',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        '#3C486B',
+        '#F9DBBB',
+        '#F9D949',
+        '#F45050',
+        '#009FBD',
+        '#210062',
+        '#F5E9CF'
+      ],
+      borderWidth: 1
+    }]
   };
-  return <Bar options={options} data={data} className="chartBar" />;
+  return <Bar  data={data} className="chartBar" options={options} />;
 }
 
 export default BarChart;
--- project-final/src/Component/Dashboard/Dashboard.js
+++ project-final/src/Component/Dashboard/Dashboard.js
@@ -7,6 +7,7 @@ import { BiTime } from "react-icons/bi";
 import BarChart from "../Chart/BarChart";
 import PieChart from "../Chart/PieChart";
 import axios from "axios";
+import { MDBDataTableV5 } from "mdbreact";
 function Dashboard() {
   const [canchk, setCanChk] = useState(null);
   const [checked, setChecked] = useState(null);
@@ -17,9 +18,9 @@ function Dashboard() {
   const month = new Date(date).getMonth() + 1;
   const currentYear = new Date(date).getFullYear();
   const [fisiyear, setFisiYear] = useState(null);
+  const [dataTest,setDataTest] = useState([])
   useEffect(() => {
     const use = [];
-
     const main = localStorage.getItem("main_aid");
     if (month >= 10) {
       setFisiYear(currentYear + 544);
@@ -66,8 +67,22 @@ function Dashboard() {
         className=" bg-white d-flex flex-xl-row flex-column  py-3 px-3  text-inf justify-content-around "
         style={{ borderRadius: 15 }}
       >
-        <div className="col-xl-4 col-lg-12 col-md-12 col-12 py-2   card-data">
+        <div className="col-xl-3 col-lg-12 col-md-12 col-12 py-2   card-data">
           <div className="box-all   h-100 p-4 card-info ">
+            <div className="col-4 icon-card  ">
+              <AiFillDatabase />
+            </div>
+            <div className="col-8 text-card  ">
+              <div className="card-text">ครุภัณฑ์ทั้งหมด</div>
+              <div className="text-incard">
+                <div className=" resualt ">{canchk}</div>
+                <div>หน่วย</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-3 col-lg-12 col-md-12 col-12 py-2   card-data">
+          <div className="box-all-use   h-100 p-4 card-info ">
             <div className="col-4 icon-card  ">
               <AiFillDatabase />
             </div>
@@ -80,7 +95,7 @@ function Dashboard() {
             </div>
           </div>
         </div>
-        <div className="col-xl-4 col-lg-12 col-md-12 col-12 py-2  card-data">
+        <div className="col-xl-3 col-lg-12 col-md-12 col-12 py-2  card-data">
           <div className="box-check   h-100 p-4 card-info ">
             <div className="col-4 icon-card  ">
               <GoChecklist />
@@ -94,7 +109,7 @@ function Dashboard() {
             </div>
           </div>
         </div>
-        <div className="col-xl-4 col-lg-12 col-md-12 col-12 py-2 card-data">
+        <div className="col-xl-3 col-lg-12 col-md-12 col-12 py-2 card-data">
           <div className="box-wait   h-100 p-4 card-info ">
             <div className="col-4 icon-card  ">
               <ImClock2 />
@@ -111,17 +126,22 @@ function Dashboard() {
       </div>
       <div className="pt-4 ">
         <div
-          className="d-flex flex-xl-row flex-column bg-white"
-          style={{ borderRadius: 15 }}
+          className="d-flex flex-xl-row flex-column"
+          style={{ borderRadius: 15,gap:20 }}
         >
-          <div className="col-12 col-xl-8" style={{ overflow: "auto" }}>
+          {/* <div className="col-12 col-xl-8" style={{ overflow: "auto" }}>
             <div className=" p-4" style={{ height: 450, width: 850 }}>
               <BarChart />
             </div>
-          </div>
-          <div className=" col-12 col-xl-4 ">
+          </div> */}
+          <div className=" col-12 col-xl-4 bg-white  " style={{ borderRadius: 15 }}>
             <div className="p-4">
               <PieChart />
+            </div>
+          </div>
+          <div className="bg-white col" style={{ borderRadius: 15 }} >
+          <div className=" p-4" style={{ height: 450, width: 850 }}>
+              <BarChart />
             </div>
           </div>
         </div>
